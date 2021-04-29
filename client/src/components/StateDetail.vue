36d21553f46bba603ec92501424a3236cd4b0541
Fernando Molano <fm87usa@gmail.com>
2021-04-29 12:41:25 -0500

    create individual component for each one of states, add styles to state components and homepage to organize list of states, add checkbox element to each state to select if it has been visited

---
+++ client/src/components/StateDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="state-info">
+        <span class="state-name">{{ state.name }}</span>
+        <div>
+            <input type="checkbox" class="visit-state">
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'StateDetail',
+    props: {
+        state: Object
+    }
+}
+</script>
+
+<style scoped>
+
+.state-info {
+    padding: 1rem;
+    height: 8rem;
+    width: 10rem;
+    border: 1px solid gainsboro;
+    border-radius: .3rem;
+    background-color: whitesmoke;
+}
+
+.visit-state {
+    margin: 1rem;
+    text-align: center;
+}
+</style>
--- client/src/components/StateList.vue
+++ client/src/components/StateList.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
-        <p v-for="state in states" v-bind:key="state.name">
-            {{ state.name }}
-        </p>
+    <div class="state-list-container">
+        <div class="state-container" v-for="state in states" v-bind:key="state.name">
+            <state-detail v-bind:state="state"></state-detail>
+        </div>
     </div>
 </template>
     
 <script>
+import StateDetail from './StateDetail.vue'
 
 export default {
+  components: { StateDetail },
     name: 'StateList',
     data() {
         return {
@@ -31,4 +33,13 @@ export default {
 
 <style scoped>
 
+.state-list-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+
+.state-container {
+    margin: 1rem;
+}
 </style>
